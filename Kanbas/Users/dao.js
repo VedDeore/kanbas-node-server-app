cd97704ec8246d69667ea5903a89664dc7e8c2b5
Ved Deore <veddeore2312@gmail.com>
2024-11-23 13:18:55 -0500

    Added atlas DB

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -1,20 +1,39 @@
-import db from "../Database/index.js";
-let { users } = db;
+import model from "./model.js";
 export const createUser = (user) => {
-  const newUser = { ...user, _id: Date.now() };
-  users = [...users, newUser];
-  return newUser;
+  delete user._id;
+  return model.create(user);
 };
-export const findAllUsers = () => users;
-export const findUserById = (userId) =>
-  users.find((user) => user._id === userId);
-export const findUserByUsername = (username) =>
-  users.find((user) => user.username === username);
-export const findUserByCredentials = (username, password) =>
-  users.find(
-    (user) => user.username === username && user.password === password
-  );
-export const updateUser = (userId, user) =>
-  (users = users.map((u) => (u._id === userId ? user : u)));
-export const deleteUser = (userId) =>
-  (users = users.filter((u) => u._id !== userId));
+export const findAllUsers = () => {
+  return model.find();
+};
+
+export const findUserById = (userId) => {
+  return model.findById(userId);
+};
+
+export const findUserByUsername = (username) => {
+  return model.findOne({ username: username });
+};
+
+export const findUserByCredentials = (username, password) => {
+  return model.findOne({ username, password });
+};
+
+export const updateUser = (userId, user) => {
+  return model.updateOne({ _id: userId }, { $set: user });
+};
+
+export const deleteUser = (userId) => {
+  return model.deleteOne({ _id: userId });
+};
+
+export const findUsersByRole = (role) => {
+  return model.find({ role: role });
+};
+
+export const findUsersByPartialName = (partialName) => {
+  const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+  return model.find({
+    $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+  });
+};
